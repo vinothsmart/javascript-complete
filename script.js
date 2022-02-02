740ca66c0d863efe542fcea5f0bd694e1b75abaf
vinothsmart <s.vinothkanna90@gmail.com>
2022-02-02 19:09:39 +0530

    Equality Operators: == vs. ===

--- script.js
+++ script.js
@@ -72,3 +72,18 @@ console.log("19" - "13" + "17"); // -> '617'
 console.log("19" - "13" + 17); // -> 23
 console.log("123" < 57); // -> false
 console.log(5 + 6 + "4" + 9 - 4 - 2); // -> 1143
+
+// const numNeighbours = prompt(
+//   "How many neighbour countries does your country have?"
+// );
+// LATER : This helps us prevent bugs
+const numNeighbours = Number(
+  prompt("How many neighbour countries does your country have?")
+);
+if (numNeighbours === 1) {
+  console.log("Only 1 border!");
+} else if (numNeighbours > 1) {
+  console.log("More than 1 border");
+} else {
+  console.log("No borders");
+}
