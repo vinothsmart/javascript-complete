372415fbf3c3997e43e58d71f75fba5f4752e7fa
vinothsmart <s.vinothkanna90@gmail.com>
2022-02-02 19:03:13 +0530

    Type Conversion and Coercion

--- script.js
+++ script.js
@@ -66,3 +66,9 @@ if (population > 33) {
     `${country}'s population is ${33 - population} million below average`
   );
 }
+
+console.log("9" - "5"); // -> 4
+console.log("19" - "13" + "17"); // -> '617'
+console.log("19" - "13" + 17); // -> 23
+console.log("123" < 57); // -> false
+console.log(5 + 6 + "4" + 9 - 4 - 2); // -> 1143
