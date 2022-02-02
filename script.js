f462a1871c426cfeb6e725704acc9492d2d56095
vinothsmart <s.vinothkanna90@gmail.com>
2022-02-02 18:59:54 +0530

    Taking Decisions: if / else Statements

--- script.js
+++ script.js
@@ -58,3 +58,11 @@ console.log(description1);
 const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
 
 console.log(description);
+
+if (population > 33) {
+  console.log(`${country}'s population is above average`);
+} else {
+  console.log(
+    `${country}'s population is ${33 - population} million below average`
+  );
+}
