43ed5d81db8bb82bc61b99be501d5f865041884f
vinothsmart <s.vinothkanna90@gmail.com>
2022-02-02 18:57:24 +0530

    Basic Operators

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ console.log(myFirstJob);
 
 // let country = "Portugal";
 // let continent = "Europe";
-// let population = 10;
+let population = 10;
 // console.log(country);
 // console.log(continent);
 // console.log(population);
@@ -39,3 +39,18 @@ console.log(language);
 console.log(country);
 console.log(continent);
 console.log(isIsland);
+
+console.log(population / 2);
+population++;
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+const description1 =
+  country +
+  " is in " +
+  continent +
+  ", and its " +
+  population +
+  " million people speak " +
+  language;
+console.log(description1);
