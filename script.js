5b028d25c8763683c3a64988a406facd41add1c1
vinothsmart <s.vinothkanna90@gmail.com>
2022-02-02 15:27:24 +0530

    variables

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ console.log(40 + 8 + 23 - 10);
 
 console.log("vinoth");
 console.log(23);
+
+let firstName = "Vinoth";
+console.log(firstName);
+
+let person = "vinoth";
+let PI = 3.1415;
+
+let myFirstJob = "android developer";
+let mycurrentJob = "web developer";
+
+console.log(myFirstJob);
