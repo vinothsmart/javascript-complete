875375108519b8f05f826c8f59facab34b085856
vinothsmart <s.vinothkanna90@gmail.com>
2022-02-02 15:30:05 +0530

    data type

--- script.js
+++ script.js
@@ -21,3 +21,10 @@ let population = 10;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
