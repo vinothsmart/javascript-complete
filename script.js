d01c2bc3c507f1a72a7bd80294aa0fca33ad46b8
vinothsmart <s.vinothkanna90@gmail.com>
2022-02-03 18:08:57 +0530

    type conversion and coercion

--- script.js
+++ script.js
@@ -154,7 +154,7 @@ if (birthYear <= 2000) {
 }
 
 console.log(century);
-*/
+
 
 const massMark = 78;
 const heightMark = 1.69;
@@ -175,3 +175,24 @@ if (BMIMark > BMIJohn) {
 } else {
   console.log(`John's BMI (${BMIMark}) is higher than Mark's (${BMIJohn})`);
 }
+*/
+
+// type conversion
+const inputYear = "1990";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Jonas"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+console.log("I am" + 23 + "years old");
+console.log("23" - "10" - 3);
+console.log("23" * 2);
+console.log("23" / 2);
+
+let n = "1" + 1;
+n = n - 1;
+console.log(n);
