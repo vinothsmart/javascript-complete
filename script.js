4b147070b9a4ec9db7033316aee3494e2cff02c8
vinothsmart <s.vinothkanna90@gmail.com>
2022-02-03 17:55:23 +0530

    execeise

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ console.log(vinothNew);
 console.log(`Just a regualr string`);
 
 console.log("String with \n mutiple \n lines");
-*/
+
 
 const age = 19;
 
@@ -154,3 +154,24 @@ if (birthYear <= 2000) {
 }
 
 console.log(century);
+*/
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})`);
+} else {
+  console.log(`John's BMI (${BMIMark}) is higher than Mark's (${BMIJohn})`);
+}
