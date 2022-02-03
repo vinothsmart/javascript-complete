ca996dc52e252335ccd49e8116900894dd127ed2
vinothsmart <s.vinothkanna90@gmail.com>
2022-02-03 11:03:13 +0530

    Basic Array Operations (Methods)

--- script.js
+++ script.js
@@ -163,3 +163,14 @@ const percentages = [
   percentageOfWorld1(populations[3]),
 ];
 console.log(percentages);
+
+const neighbours = ["Norway", "Sweden", "Russia"];
+neighbours.push("Utopia");
+console.log(neighbours);
+neighbours.pop();
+console.log(neighbours);
+if (!neighbours.includes("Germany")) {
+  console.log("Probably not a central European country :D");
+}
+neighbours[neighbours.indexOf("Sweden")] = "Republic of Sweden";
+console.log(neighbours);
