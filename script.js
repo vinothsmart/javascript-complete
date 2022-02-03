31d07e9d135372b43b16438870dcbcbba5545e86
vinothsmart <s.vinothkanna90@gmail.com>
2022-02-03 10:54:11 +0530

    functions

--- script.js
+++ script.js
@@ -118,3 +118,11 @@ switch (language) {
 console.log(
   `${country}'s population is ${population > 33 ? "above" : "below"} average`
 );
+
+function describeCountry(country, population, capitalCity) {
+  return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+}
+const descPortugal = describeCountry("Portugal", 10, "Lisbon");
+const descGermany = describeCountry("Germany", 83, "Berlin");
+const descFinland = describeCountry("Finland", 6, "Helsinki");
+console.log(descPortugal, descGermany, descFinland);
