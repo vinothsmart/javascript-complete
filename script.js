ed8fe9d440bc85b7193b57daea4f9ffc4af5d833
vinothsmart <s.vinothkanna90@gmail.com>
2022-02-03 10:56:13 +0530

    Function Declarations vs. Expressions

--- script.js
+++ script.js
@@ -126,3 +126,14 @@ const descPortugal = describeCountry("Portugal", 10, "Lisbon");
 const descGermany = describeCountry("Germany", 83, "Berlin");
 const descFinland = describeCountry("Finland", 6, "Helsinki");
 console.log(descPortugal, descGermany, descFinland);
+
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+const percentageOfWorld2 = function (population) {
+  return (population / 7900) * 100;
+};
+const percPortugal1 = percentageOfWorld1(10);
+const percChina1 = percentageOfWorld1(1441);
+const percUSA1 = percentageOfWorld1(332);
+console.log(percPortugal1, percChina1, percUSA1);
