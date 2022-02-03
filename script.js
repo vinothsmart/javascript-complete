4a5cc06c7ce962fb8e9436afb7e40c37bfd8696d
vinothsmart <s.vinothkanna90@gmail.com>
2022-02-03 11:11:30 +0530

    Dot vs. Bracket Notation

--- script.js
+++ script.js
@@ -182,3 +182,14 @@ const myCountry = {
   population: 6,
   neighbours: ["Norway", "Sweden", "Russia"],
 };
+
+console.log(
+  `${myCountry.country} has ${myCountry.population} million
+  ${myCountry.language}-speaking people,
+  ${myCountry.neighbours.length} neighbouring countries and
+  a capital called ${myCountry.capital}.`
+);
+myCountry.population += 2;
+console.log(myCountry.population);
+myCountry["population"] -= 2;
+console.log(myCountry.population);
