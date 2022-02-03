bc76f9848ba254e2e9794635e7b60cbba995b230
vinothsmart <s.vinothkanna90@gmail.com>
2022-02-03 11:01:14 +0530

    Introduction to Arrays

--- script.js
+++ script.js
@@ -153,3 +153,13 @@ const describePopulation = function (country, population) {
 describePopulation("Portugal", 10);
 describePopulation("China", 1441);
 describePopulation("USA", 332);
+
+const populations = [10, 1441, 332, 83];
+console.log(populations.length === 4);
+const percentages = [
+  percentageOfWorld1(populations[0]),
+  percentageOfWorld1(populations[1]),
+  percentageOfWorld1(populations[2]),
+  percentageOfWorld1(populations[3]),
+];
+console.log(percentages);
