959e3145577bae3a9909ef5b565800cdb60e29be
vinothsmart <s.vinothkanna90@gmail.com>
2022-02-03 10:56:48 +0530

    Arrow Functions

--- script.js
+++ script.js
@@ -137,3 +137,9 @@ const percPortugal1 = percentageOfWorld1(10);
 const percChina1 = percentageOfWorld1(1441);
 const percUSA1 = percentageOfWorld1(332);
 console.log(percPortugal1, percChina1, percUSA1);
+
+const percentageOfWorld3 = (population) => (population / 7900) * 100;
+const percPortugal3 = percentageOfWorld3(10);
+const percChina3 = percentageOfWorld3(1441);
+const percUSA3 = percentageOfWorld3(332);
+console.log(percPortugal3, percChina3, percUSA3);
