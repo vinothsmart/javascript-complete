59f92dfcc67ba92256ba37fa39f5a7f5f3505c9b
vinothsmart <s.vinothkanna90@gmail.com>
2022-02-23 19:49:06 +0530

    array code challenge

--- script.js
+++ script.js
@@ -170,7 +170,7 @@ console.log(age1, age2, age3);
 
 const ages = [calAge(ageYears[0]), calAge(ageYears[1]), calAge(ageYears[2])];
 console.log(ages);
-*/
+
 
 const friends = ["Sandhiya", "Ruban", "Mari"];
 
@@ -203,3 +203,15 @@ console.log(friends.includes(23));
 if (friends.includes("Ruban")) {
   console.log("Ruban is a friend");
 }
+*/
+
+const calcTip = (bill) =>
+  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const bills = [125, 555, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(bills, tips);
+console.log(totals);
