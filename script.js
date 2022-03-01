0bcad5bafb9d2b26ddbc7e6406f381aa53ba05b2
vinothsmart <s.vinothkanna90@gmail.com>
2022-03-01 19:08:19 +0530

    object method challenge

--- script.js
+++ script.js
@@ -263,7 +263,6 @@ console.log(vinoth);
 console.log(
   `${vinoth.firstName} has ${vinoth.friends.length}, and his best friend is ${vinoth.friends[0]}`
 );
-*/
 
 const vinoth = {
   firstName: "Vinoth",
@@ -290,3 +289,35 @@ console.log(vinoth.age);
 
 // vinoth is a 32 years old Senior Software Developer he has drivers license
 console.log(vinoth.getSummary());
+*/
+
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+mark.calcBMI();
+john.calcBMI();
+console.log(mark.bmi, john.bmi);
+
+if (mark.bmi > john.bmi) {
+  console.log(`${mark.fullName} has a higher BMI`);
+} else if (mark.bmi < john.bmi) {
+  console.log(`${john.fullName} has a higher BMI`);
+} else {
+  console.log(`Both have same BMI`);
+}
