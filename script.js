e7963dd9f067461a12493e66dbf868da8f7aea46
vinothsmart <s.vinothkanna90@gmail.com>
2022-03-02 16:20:12 +0530

    code challege in loops

--- script.js
+++ script.js
@@ -392,7 +392,7 @@ for (let execise = 1; execise <= 5; execise++) {
     console.log(`Lifting weight repetition ${rep}`);
   }
 }
-*/
+
 
 let rep = 1;
 while (rep <= 10) {
@@ -410,3 +410,31 @@ while (dice !== 6) {
     console.log("You win!");
   }
 }
+*/
+
+const calcTip = (bill) =>
+  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  tips.push(calcTip(bills[i]));
+  totals.push(bills[i] + tips[i]);
+}
+
+console.log(bills, tips, totals);
+
+const calcAverage = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // sum = sum + arr[i];
+    sum += arr[i];
+  }
+  // console.log(sum);
+  return sum / arr.length;
+};
+console.log(calcAverage([2, 3, 6]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
