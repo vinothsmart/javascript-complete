3ce05928f17db7c5cf36a1301c3823f2efd8e501
vinothsmart <s.vinothkanna90@gmail.com>
2022-03-03 15:04:18 +0530

    mdn

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
 // Remember , we're gonna use srtice in all scripts now
 'use strict';
 
-const x = '23';
-if (x === 23) console.log(23);
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
-const calcAge = birthYear => 2022 - birthYear;
-console.log(calcAge(1990));
+const calcTempAmplitude = temps => {
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const currentTemp = temps[i];
+    if (typeof currentTemp !== 'number') continue;
+    if (currentTemp > max) max = currentTemp;
+    if (currentTemp < min) min = currentTemp;
+  }
+  return max - min;
+};
+
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
+
+const calcTempAmplitudeNew = (t1, t2) => {
+  const temps = [...t1, ...t2];
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const currentTemp = temps[i];
+    if (typeof currentTemp !== 'number') continue;
+    if (currentTemp > max) max = currentTemp;
+    if (currentTemp < min) min = currentTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [6, 7, 8, 9, 10];
+const mergeArray = [...array1, ...array2];
+
+console.log(mergeArray);
+
+const checking = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+
+console.log(checking);
