72c9aac6722409e31cd397abfe93b3d2e9cf6471
vinothsmart <s.vinothkanna90@gmail.com>
2022-03-09 14:58:30 +0530

    debugger

--- script.js
+++ script.js
@@ -47,7 +47,8 @@ const measureKelvin = () => {
   const measurement = {
     type: 'temp',
     unit: 'celsius',
-    value: Number(prompt('Enter temperature in celsius')),
+    // value: Number(prompt('Enter temperature in celsius')),
+    value: 10,
   };
 
   // console.log(measurement);
@@ -59,3 +60,28 @@ const measureKelvin = () => {
 };
 
 console.log(measureKelvin());
+
+// Using Debugger
+const calcTempAmplitudeNewBug = (t1, t2) => {
+  const temps = [...t1, ...t2];
+  let max = 0;
+  let min = 0;
+  for (let i = 0; i < temps.length; i++) {
+    const currentTemp = temps[i];
+    if (typeof currentTemp !== 'number') continue;
+    if (currentTemp > max) max = currentTemp;
+    if (currentTemp < min) min = currentTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [6, 7, 8, 9, 10];
+const mergeArray = [...array1, ...array2];
+
+console.log(mergeArray);
+
+const amplitudeBug = calcTempAmplitudeNewBug([3, 5, 1], [9, 4, 5]);
+
+console.log(amplitudeBug);
