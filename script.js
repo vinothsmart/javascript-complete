ddd1713eeab473a1acfb3757dafe7c9cb01fba41
vinothsmart <s.vinothkanna90@gmail.com>
2022-03-11 18:26:15 +0530

    manipulating css

--- script.js
+++ script.js
@@ -15,30 +15,39 @@ console.log(document.querySelector('.guess').value);
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  // when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number';
+
+    // when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number';
+    document.querySelector('.number').textContent = guess;
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').style.width = '30rem';
+
+    // when guess is too high
   } else if (guess > secretNumber) {
     if (score > 0) {
       document.querySelector('.message').textContent = 'Too High';
       score--;
-      document.querySelector('.message').textContent = score;
+      document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = 'Game Over';
       document.querySelector('.score').textContent = 0;
     }
+
+    // when guess is too low
   } else {
     if (score > 0) {
       document.querySelector('.message').textContent = 'Too Low';
       score--;
-      document.querySelector('.message').textContent = score;
+      document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = 'Game Over';
       document.querySelector('.score').textContent = 0;
