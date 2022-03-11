881fc41293ce127880d5440331586e3919b8694e
vinothsmart <s.vinothkanna90@gmail.com>
2022-03-11 17:49:55 +0530

    game logic

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const x = function () {
-  console.log(23);
-};
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -23,5 +23,25 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number';
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too High';
+      score--;
+      document.querySelector('.message').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Game Over';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too Low';
+      score--;
+      document.querySelector('.message').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Game Over';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
