7d605baa18c44389ecfffec2237cf4cbd6a440f3
vinothsmart <s.vinothkanna90@gmail.com>
2022-03-11 11:52:47 +0530

    handle click

--- script.js
+++ script.js
@@ -13,6 +13,15 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+const x = function () {
+  console.log(23);
+};
+
 document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number';
+  }
 });
