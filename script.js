8b9665d5270cd79eebfad71c6aa755c2d1ce0266
vinothsmart <s.vinothkanna90@gmail.com>
2022-03-14 13:06:16 +0530

    highscore

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -30,6 +31,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = 'green';
     document.querySelector('.number').style.width = '30rem';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
 
     // when guess is too high
   } else if (guess > secretNumber) {
