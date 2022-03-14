2610ef9c4e35c46d6f3992a27f9367ed1e5e9d99
vinothsmart <s.vinothkanna90@gmail.com>
2022-03-14 16:20:50 +0530

    model list

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const model = document.querySelector('.model');
+const overlay = document.querySelector('.overlay');
+const btnCloseModel = document.querySelector('.close-modal');
+const btnOpenModel = document.querySelectorAll('.show-modal');
+console.log(btnOpenModel);
+
+for (let i = 0; i < btnOpenModel.length; i++)
+  console.log(btnOpenModel[i].textContent);
